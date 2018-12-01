84704cc7f0e22e6d09d041f6db5c3399a16e7aef
juliof9 <00081817@uca.edu.sv>
2018-12-01 17:53:11 -0600

    Avanzando Todavia

--- app.js
+++ app.js
@@ -17,6 +17,29 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/3000', {useNewUrlParser: true})
+        .then(() => {
+            console.log("Connect Success!");
+        })
+        .catch(err => {
+            console.log("No se pudo establecer la conexcion a la base de datos");
+            console.log(err);
+        });
+
+var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use('/', logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 app.use(function(req, res, next){
     next(createError(404));
 });
--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-var UserModel = require('/..models/User');
+var UserModel = require('../models/User');
 
 let controller = {};
 
